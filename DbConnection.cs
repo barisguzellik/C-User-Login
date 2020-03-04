b4a2788e26d1898574c6cfd45dd6263a7f1dccab
Barış Güzellik <barisguzellik@gmail.com>
2020-03-04 16:44:08 +0300

    Create DbConnection.cs

---
+++ DbConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Project.Class
+{
+    public class DbConnection
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["sqlContext"].ConnectionString);
+        public bool login(string username, string password)
+        {
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT*FROM USERS WHERE USERNAME=@USERNAME AND PASSWORD=@PASSWORD AND STATUS=1", con);
+            dataAdapter.SelectCommand.Parameters.AddWithValue("@USERNAME", username);
+            dataAdapter.SelectCommand.Parameters.AddWithValue("@PASSWORD", MD5Hash(password));
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            if (dataTable.Rows.Count == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public string MD5Hash(string val)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = ASCIIEncoding.Default.GetBytes(val);
+            byte[] encoded = md5.ComputeHash(bytes);
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < encoded.Length; i++)
+                strBuilder.Append(encoded[i].ToString("x2"));
+            return strBuilder.ToString();
+        }
+    }
+}
